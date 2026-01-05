47a0fc555819051676c610e344f7441d479e5319
zorothecoder00 <awledouamoussou95@gmail.com>
2026-01-05 15:59:25 +0000

    Ajout de prisma generate dans l'éxcécution du build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,21 @@
 import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { Pool } from "pg"   
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}  
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ["query"] })
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg(pool),
+    log: ["query"],
+  })
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
