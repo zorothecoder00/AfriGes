5f8d3d777e84c1f21d213a9b8875f6bb739836ca
zorothecoder00 <awledouamoussou95@gmail.com>
2026-01-13 10:34:23 +0000

    Correction de la page d'accueil et redirection selon le token et le rôle

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-// app/page.tsx (server)
-import DashboardClient from "@/components/DashboardClient";   
+import { redirect } from "next/navigation"
+import { getAuthSession } from "@/lib/auth"
+import { Role } from "@/types"
 
-export default function Page() {
-  return <DashboardClient />;      
+export default async function Home() {
+  const session = await getAuthSession()
+
+  // Pas connecté
+  if (!session) {
+    redirect("/auth/login")
+  }
+
+  const role = session.user.role
+
+  switch (role) {
+    case Role.SUPER_ADMIN:
+    case Role.ADMIN:
+      redirect("/dashboard/admin")
+
+    case Role.USER:
+      redirect("/dashboard/user")
+
+    default:
+      redirect("/unauthorized")
+  }
 }
-   
+
+
